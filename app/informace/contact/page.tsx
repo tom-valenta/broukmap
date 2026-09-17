8b033f64a69997aec6e417ee6cc3e25efe7f9a5e
Tomáš Valenta <tomas.valenta255@gmail.com>
2026-09-17 13:42:05 +0200

    add placeholder content to informace pages

---
+++ app/informace/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function ContactPage() {
+    return (
+        <div>
+            <h1>Contact Us</h1>
+            <p>If you have any questions or feedback, feel free to reach out to us!</p>
+        </div>
+    );
+}
---
+++ app/informace/termsOfUse/page.tsx
@@ -0,0 +1,8 @@
+export default function TermsOfUsePage() {
+    return (
+        <div>
+            <h1>Terms of Use</h1>
+            <p>By using this website, you agree to comply with our terms and conditions. Please read them carefully.</p>
+        </div>
+    );
+}
